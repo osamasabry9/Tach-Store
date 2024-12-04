6bde405b70054e704ca29ce64ac5209858bf6ee5
osamasabry9 <osamasabry324@gmail.com>
2024-12-04 22:19:31 +0400

    Add store of category { Thunk and slice}

---
+++ src/store/categories/categoriesSlice.ts
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+import actGetCategories from "./act/actGetCategories";
+import { TCategory } from "@customTypes/category";
+import { TLoading } from "@customTypes/shared";
+
+// Define a type for the slice state
+interface ICategoriesState {
+  records: TCategory[];
+  loading: TLoading;
+  error: string | null;
+}
+
+const initialState: ICategoriesState = {
+  records: [],
+  loading: "idle",
+  error: null,
+};
+
+const categoriesSlice = createSlice({
+  name: "categories",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(actGetCategories.pending, (state) => {
+        state.loading = "pending";
+        state.error = null;
+      })
+      .addCase(actGetCategories.fulfilled, (state, action) => {
+        state.loading = "succeeded";
+        state.records = action.payload;
+      })
+      .addCase(actGetCategories.rejected, (state, action) => {
+        state.loading = "failed";
+        if (action.payload && typeof action.payload === "string") {
+          state.error = action.payload;
+        }
+      });
+  },
+});
+
+export { actGetCategories };
+export default categoriesSlice.reducer;
