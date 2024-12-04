c9985f5507227bcc654124eeb52cb6cc2d48d573
osamasabry9 <osamasabry324@gmail.com>
2024-12-04 15:17:37 +0400

    Add router from provider app router

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import MainLayout from "@layouts/MainLayout/MainLayout";
+import AppRouter from "@routes/AppRouter";
 
-createRoot(document.getElementById("root")!).render(<MainLayout />);
+// styles
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/global.css";
+
+createRoot(document.getElementById("root")!).render(<AppRouter />);
