6a09079112865108f435652265b497704e517346
osamasabry9 <osamasabry324@gmail.com>
2024-12-04 15:15:48 +0400

    Add Outlet from react-router-dom and display custom component in main layout

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { Container } from "react-bootstrap";
 import style from "./style.module.css";
 import { Header, Footer } from "@components/common";
+import { Outlet } from "react-router-dom";
 
 const { container, wrapper } = style;
 
@@ -8,7 +9,9 @@ const MainLayout = () => {
   return (
     <Container className={container}>
       <Header />
-      <main className={wrapper}></main>
+      <main className={wrapper}>
+        <Outlet/>
+      </main>
       <Footer />
     </Container>
   );
