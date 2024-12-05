19dcd9e57add05aaab1023948b4e6031c0c99551
osamasabry9 <osamasabry324@gmail.com>
2024-12-05 23:50:27 +0400

    Use GridList into pages to display data

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,9 +1,12 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import { Product } from "@components/eCommerce";
 import { useAppDispatch, useAppSelector } from "@store/hooks";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { actGetProducts, productsCleanUp } from "@store/products/productSlice";
+import { Loading } from "@components/feedback";
+import { GridList } from "@components/common";
+import { TProduct } from "@customTypes/product";
 
 const Products = () => {
   const { prefix } = useParams();
@@ -15,28 +18,17 @@ const Products = () => {
       dispatch(productsCleanUp());
     };
   }, [dispatch]);
-  if (loading === "pending") {
-    return <h1>Loading...</h1>;
-  }
-  if (error) {
-    return <h1>{error}</h1>;
-  }
+
   const ListOfProducts = records.length > 0 ? records : [];
 
   return (
     <Container>
-      <Row>
-        {ListOfProducts.map((product) => (
-          <Col
-            key={product.id}
-            xs={6}
-            md={3}
-            className="d-flex justify-content-center mb-4 mt-2 "
-          >
-            <Product {...product} />
-          </Col>
-        ))}
-      </Row>
+      <Loading loading={loading} error={error}>
+        <GridList<TProduct>
+          records={ListOfProducts}
+          renderItem={(record) => <Product {...record} />}
+        />
+      </Loading>
     </Container>
   );
 };
