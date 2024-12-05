0c815ed653d1de4dcc0902e21747256a83d581e7
osamasabry9 <osamasabry324@gmail.com>
2024-12-05 23:49:59 +0400

    Add Loading component

---
+++ src/components/feedback/Loading/Loading.tsx
@@ -0,0 +1,26 @@
+import { TLoading } from "@customTypes/shared";
+import Styles from "./styles.module.css";
+
+const { load, spinner } = Styles;
+
+type LoadingProps = {
+  loading: TLoading;
+  error: string | null;
+  children: React.ReactNode;
+};
+const Loading = ({ loading, error, children }: LoadingProps) => {
+  if (loading === "pending") {
+    return (
+      <div className={load}>
+        <div className={spinner}></div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+  if (loading === "failed") {
+    return <div>{error}</div>;
+  }
+  return <div>{children}</div>;
+};
+
+export default Loading;
