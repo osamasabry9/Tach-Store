3ddbeb3323d97d1b9f43acfa811d6ee63f5c8bb7
osamasabry9 <osamasabry324@gmail.com>
2024-12-18 16:05:32 +0400

    use cart store in header

--- src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
+++ src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
@@ -1,13 +1,17 @@
 import Logo from "@assets/Icons/iconBasket.svg?react";
+import { useAppSelector } from "@store/hooks";
+import { getCartTotalQuantitySelector } from "@store/cart/selectors";
 import styles from "./styles.module.css";
 
 const { basketContainer, basketQuantity, basketIcon } = styles;
 
 const HeaderBasket = () => {
+  const totalQuantity = useAppSelector(getCartTotalQuantitySelector);
+
   return (
     <div className={basketContainer}>
       <Logo title="basket icon" className={basketIcon} />
-      <div className={basketQuantity}>0</div>
+      <div className={basketQuantity}>{totalQuantity}</div>
     </div>
   );
 };
