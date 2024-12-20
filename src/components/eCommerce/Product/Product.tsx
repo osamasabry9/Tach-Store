e011004d03e1ebd5481f1eaa7aa0094b6132a266
osamasabry9 <osamasabry324@gmail.com>
2024-12-20 11:52:10 +0400

    Add quantity reached to max  to control disabled button Add  Remaining of items

--- src/components/eCommerce/Product/Product.tsx
+++ src/components/eCommerce/Product/Product.tsx
@@ -1,16 +1,31 @@
-import { Button } from "react-bootstrap";
+import { useState, useEffect, memo } from "react";
+import { Button, Spinner } from "react-bootstrap";
 import { TProduct } from "@customTypes/product";
 import { useAppDispatch } from "@store/hooks";
 import { addToCart } from "@store/cart/cartSlice";
 
 import styles from "./styles.module.css";
-const { product, productImg } = styles;
 
-const Product = ({ id, title, price, img }: TProduct) => {
+const { product, productImg, maximumNotice } = styles;
+
+const Product = memo(({ id, title, price, img, max, quantity }: TProduct) => {
   const dispatch = useAppDispatch();
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const currentRemainingQuantity = max - (quantity ?? 0);
+  const quantityReachedToMax = currentRemainingQuantity <= 0 ? true : false;
+
+  useEffect(() => {
+    if (!isDisabled) return;
+    const debounce = setTimeout(() => {
+      setIsDisabled(false);
+    }, 300);
+    return () => clearTimeout(debounce);
+  }, [isDisabled]);
 
   const handleAddToCart = () => {
-    dispatch(addToCart({ id}));
+    dispatch(addToCart(id));
+    setIsDisabled(true);
   };
 
   return (
@@ -19,12 +34,27 @@ const Product = ({ id, title, price, img }: TProduct) => {
         <img src={img} alt={title} />
       </div>
       <h2>{title}</h2>
-      <h3>{price} EGP</h3>
-      <Button variant="info" style={{ color: "white" }} onClick={handleAddToCart}>
-        Add to cart
+      <h3>{price.toFixed(2)} EGP</h3>
+      <p className={maximumNotice}>Remaining: {currentRemainingQuantity}</p>
+      {quantityReachedToMax && (
+        <p className={maximumNotice}>Quantity reached to max</p>
+      )}
+      <Button
+        variant="info"
+        style={{ color: "white" }}
+        onClick={handleAddToCart}
+        disabled={isDisabled || quantityReachedToMax}
+      >
+        {isDisabled ? (
+          <>
+            <Spinner animation="border" size="sm" /> Loading
+          </>
+        ) : (
+          "Add to cart"
+        )}
       </Button>
     </div>
   );
-};
+});
 
 export default Product;
