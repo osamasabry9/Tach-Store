3aef890636e5ac21d98fc534ea74d44b2bb19709
osamasabry9 <osamasabry324@gmail.com>
2024-12-20 11:49:16 +0400

    Use  getCartTotalQuantitySelector from cart store Add animation when put item in basket

--- src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
+++ src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
@@ -1,17 +1,34 @@
+import { useState, useEffect } from "react";
 import Logo from "@assets/Icons/iconBasket.svg?react";
 import { useAppSelector } from "@store/hooks";
-import { getCartTotalQuantitySelector } from "@store/cart/selectors";
-import styles from "./styles.module.css";
 
-const { basketContainer, basketQuantity, basketIcon } = styles;
+import styles from "./styles.module.css";
+import { getCartTotalQuantitySelector } from "@store/cart/cartSlice";
+
+const { basketContainer, basketQuantity, basketIcon, pumpCartQuantity } =
+  styles;
 
 const HeaderBasket = () => {
+  const [isAnimate, setIsAnimate] = useState(false);
   const totalQuantity = useAppSelector(getCartTotalQuantitySelector);
+  const quantityStyle = `${basketQuantity} ${
+    isAnimate ? pumpCartQuantity : ""
+  }`;
+
+  useEffect(() => {
+    if (!totalQuantity) return;
+    setIsAnimate(true);
+    const debounce = setTimeout(() => {
+      setIsAnimate(false);
+    }, 300);
+
+    return () => clearTimeout(debounce);
+  }, [totalQuantity]);
 
   return (
     <div className={basketContainer}>
       <Logo title="basket icon" className={basketIcon} />
-      <div className={basketQuantity}>{totalQuantity}</div>
+      <div className={quantityStyle}>{totalQuantity}</div>
     </div>
   );
 };
