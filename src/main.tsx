8ed89a476690164c7c9b19e6f9cb394488ccf680
osamasabry9 <osamasabry324@gmail.com>
2024-12-20 11:44:55 +0400

    Add Persist to store and use this to storage cart items in local storage

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import { createRoot } from "react-dom/client";
 import AppRouter from "@routes/AppRouter";
 // redux
+import { store, persistor } from "@store/index";
 import { Provider } from "react-redux";
-import { store } from "@store/index";
+import { PersistGate } from "redux-persist/integration/react";
 
+// axios
+import "./services/axios-global";
 // styles
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/global.css";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
+  <PersistGate loading={null} persistor={persistor}>
     <AppRouter />
-  </Provider>
+  </PersistGate>
+</Provider>
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,57 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import categories from "./categories/categoriesSlice";
 import products from "./products/productSlice";
 import cart from "./cart/cartSlice";
 
-export const store = configureStore({
-  reducer: {
-    categories,
-    products,
-    cart,
-  },
+/* 1 - persist config selected reducers and type of storage and key
+const rootPersistConfig = {
+  key: 'root',
+  storage,
+  // list of reducers use when we want to persist save state
+  whitelist: ['cart'],
+  // black list of reducers  use when we don't want to persist save state
+ // blacklist: ['products', 'categories'],
+}
+ */
+
+const cartPersistConfig = {
+  key: 'cart',
+  storage,
+  whitelist: ['items'],
+}
+
+
+ // 2 - create root reducer
+const rootReducer = combineReducers({
+  categories,
+  products,
+  cart : persistReducer(cartPersistConfig, cart),
+});
+
+
+// 3 - create persisted reducer
+// const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
+
+// 4 - create store
+ const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+// 5 - create persistor
+const persistor = persistStore(store);
+
+// 6 - export
+export { store, persistor };
