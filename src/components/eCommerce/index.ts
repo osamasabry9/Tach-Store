ceea53f5b05818b1bfa13eca85b3d4b6ffd8adc5
osamasabry9 <osamasabry324@gmail.com>
2024-12-22 15:11:32 +0400

    Add CartItem Component and style Add CartItemList Component and style Add CartSubtotalPrice Component and style

--- src/components/eCommerce/index.ts
+++ src/components/eCommerce/index.ts
@@ -1,5 +1,8 @@
 import HeaderBasket from "./HeaderBasket/HeaderBasket";
 import Category from "./Category/Category";
 import Product from "./Product/Product";
+import CartItem from "./CartItem/CartItem";
+import CartSubtotalPrice from "./CartSubtotalPrice/CartSubtotalPrice";
+import CartItemList from "./CartItemList/CartItemList";
 
-export { HeaderBasket, Product, Category };
+export { HeaderBasket, Product, Category, CartItem, CartSubtotalPrice, CartItemList };
