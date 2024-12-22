cc3dbea5cc47e853aa297e137b5052f843bb78b0
osamasabry9 <osamasabry324@gmail.com>
2024-12-23 00:11:51 +0400

    Create HeaderWishlist component

---
+++ src/components/eCommerce/HeaderWishlist/HeaderWishlist.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import Logo from "@assets/Icons/wishlist.svg?react";
+import { useNavigate } from "react-router-dom";
+
+
+
+import styles from "./styles.module.css";
+const { container, totalNum, pumpAnimate, iconWrapper } = styles;
+
+const HeaderWishlist = () => {
+  const navigate = useNavigate();
+  const [isAnimate, setIsAnimate] = useState(false);
+  const totalQuantity = 2;
+  const quantityStyle = `${totalNum} ${
+    isAnimate ? pumpAnimate : ""
+  }`;
+
+  useEffect(() => {
+    if (!totalQuantity) return;
+    setIsAnimate(true);
+    const debounce = setTimeout(() => {
+      setIsAnimate(false);
+    }, 300);
+
+    return () => clearTimeout(debounce);
+  }, [totalQuantity]);
+
+  return (
+    <div className={container} onClick={() => navigate("/wishlist")}>
+       <div className={iconWrapper}>
+        <Logo title="Wishlist icon" />
+        {totalQuantity > 0 && (
+          <div className={quantityStyle}>{totalQuantity}</div>
+        )}
+      </div>
+      <h3>Wishlist</h3>
+    </div>
+  );
+};
+
+export default HeaderWishlist;
--- src/components/eCommerce/index.ts
+++ src/components/eCommerce/index.ts
@@ -1,8 +1,9 @@
 import HeaderBasket from "./HeaderBasket/HeaderBasket";
+import HeaderWishlist from "./HeaderWishlist/HeaderWishlist";
 import Category from "./Category/Category";
 import Product from "./Product/Product";
 import CartItem from "./CartItem/CartItem";
 import CartSubtotalPrice from "./CartSubtotalPrice/CartSubtotalPrice";
 import CartItemList from "./CartItemList/CartItemList";
 
-export { HeaderBasket, Product, Category, CartItem, CartSubtotalPrice, CartItemList };
+export { HeaderBasket, HeaderWishlist, Product, Category, CartItem, CartSubtotalPrice, CartItemList };
