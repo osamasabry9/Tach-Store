e4a28b59567247607e9768fc90f7dc1d5dc8fab4
osamasabry9 <osamasabry324@gmail.com>
2024-12-24 21:22:34 +0400

    Add wishlist page and display products  and add route in app router

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -11,6 +11,7 @@ import Login from "@pages/Login";
 import Register from "@pages/Register";
 import Error from "@pages/Error";
 import CartPage from "@pages/Cart";
+import Wishlist from "@pages/Wishlist";
 
 // Router
 const router = createBrowserRouter([
@@ -41,6 +42,7 @@ const router = createBrowserRouter([
       { path: "login", element: <Login /> },
       { path: "register", element: <Register /> },
       { path: "cart", element: <CartPage /> },
+      { path: "wishlist", element: <Wishlist /> },
     ],
   },
 ]);
