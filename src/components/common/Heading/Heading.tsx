8266978ddd24bae937d96f432a8d5c1f66a2c05f
osamasabry9 <osamasabry324@gmail.com>
2024-12-26 20:28:25 +0400

    Change heading props and also use memo

--- src/components/common/Heading/Heading.tsx
+++ src/components/common/Heading/Heading.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import { memo } from "react";
 
-const Heading = ({ children }: { children: React.ReactNode }) => {
+const Heading = memo(({ title }: { title: string }) => {
   return (
     <h2 className="mb-3 " style={{ fontSize: "26px" }}>
-      {children}
+      {title}
     </h2>
   );
-};
+});
 
 export default Heading;
