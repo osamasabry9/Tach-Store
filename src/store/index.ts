9e0cad33b64244bd517b23e7b1fa6d2729e1d645
osamasabry9 <osamasabry324@gmail.com>
2025-01-16 10:38:18 +0400

    Add Toasts slice and reducers in store

--- src/store/index.ts
+++ src/store/index.ts
@@ -16,6 +16,7 @@ import cart from "./cart/cartSlice";
 import wishlist from "./wishlist/wishlistSlice";
 import auth from "./auth/authSlice";
 import orders from "./orders/ordersSlice";
+import toasts from "./toasts/toastsSlice";
 
 /* 1 - persist config selected reducers and type of storage and key
 const rootPersistConfig = {
@@ -59,6 +60,7 @@ const rootReducer = combineReducers({
   categories,
   products,
   orders,
+  toasts,
   cart: persistReducer(cartPersistConfig, cart),
   wishlist,
 });
@@ -73,11 +75,19 @@ const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        ignoredActions: [
+          FLUSH,
+          REHYDRATE,
+          PAUSE,
+          PERSIST,
+          PURGE,
+          REGISTER,
+          "toasts/addToast",
+        ],
+        ignoredPaths: [/^toasts\.records\.\d+\.onCloseToast$/],
       },
     }),
 });
-
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
